c253a0166eba88eb78565fbe6a43da5a7a7ddc7d
osan34 <rosae619@gmail.com>
2014-03-24 17:20:27 +0545

    SportsStore Day1

--- SportsStore.WebUI/App_Start/RouteConfig.cs
+++ SportsStore.WebUI/App_Start/RouteConfig.cs
@@ -14,9 +14,44 @@ namespace SportsStore.WebUI
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
             routes.MapRoute(
+                null,
+                "",
+                new{controller = "Product", action = "List", category = (string)null, page = 1}
+            );
+
+            routes.MapRoute(
+                name: null,
+                url: "Page{page}",
+                defaults: new { Controller = "Product", action = "List" }
+            );
+
+            routes.MapRoute(null,
+"Page{page}",
+new
+{
+    controller = "Product",
+    action = "List",
+    category =
+        (string)null
+},
+new { page = @"\d+" }
+);
+
+            routes.MapRoute(null,
+"{category}",
+new { controller = "Product", action = "List", page = 1 }
+);
+            routes.MapRoute(null,
+            "{category}/Page{page}",
+            new { controller = "Product", action = "List" },
+            new { page = @"\d+" }
+            );
+            routes.MapRoute(null, "{controller}/{action}");
+
+            routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Product", action = "List", id = UrlParameter.Optional }
             );
         }
     }
---
+++ SportsStore.WebUI/Infrastructure/NinjectDependencyResolver.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+using Moq;
+using Ninject;
+using SportsStore.Domain.Abstract;
+using SportsStore.Domain.Entities;
+using SportsStore.Domain.Concrete;
+
+namespace SportsStore.WebUI.Infrastructure
+{
+    public class NinjectDependencyResolver : IDependencyResolver
+    {
+        private IKernel kernel;
+        public NinjectDependencyResolver(IKernel kernelParam)
+        {
+            kernel = kernelParam;
+            AddBindings();
+        }
+        public object GetService(Type serviceType)
+        {
+            return kernel.TryGet(serviceType);
+        }
+        public IEnumerable<object> GetServices(Type serviceType)
+        {
+            return kernel.GetAll(serviceType);
+        }
+        private void AddBindings()
+        {
+            //Mock<IProductsRepository> mock = new Mock<SportsStore.Domain.Abstract.IProductsRepository>();
+            //mock.Setup(m => m.Products).Returns(new List<Product> {
+            //new Product { Name = "Football", Price = 25 },
+            //new Product { Name = "Surf board", Price = 179 },
+            //new Product { Name = "Running shoes", Price = 95 }
+            //});
+            //kernel.Bind<IProductsRepository>().ToConstant(mock.Object);
+
+            kernel.Bind<IProductsRepository>().To<EFProductRepository>();
+        }
+    }
+}
